7ea5d1d05052309881d71b6f7acfcf1705ac0a55
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-05-13 22:40:05 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,46 +4,46 @@
 Train::Train() : countOp(0), first(nullptr) {}
 
 int Train::getOpCount() {
-	return countOp;
+        return countOp;
 }
 
 void Train::addCage(bool light) {
-	Cage* nc = new Cage;
-	if (first == nullptr) {
-		first = nc;
-		first->next = first;
-		first->prev = first;
-	} else {
-		nc->next = first;
-		nc->prev = first->prev;
-		first->prev = nc;
-		nc->prev->next = nc;
-  }
-	nc->light = light;
+        Cage* nc = new Cage;
+        if (first == nullptr) {
+                first = nc;
+                first->next = first;
+                first->prev = first;
+        } else {
+                nc->next = first;
+                nc->prev = first->prev;
+                first->prev = nc;
+                nc->prev->next = nc;
+        }
+        nc->light = light;
 }
 
 int Train::getLength() {
-	Cage* ptr = first;
-	ptr->light = 1;
-	int count = 1;
-	while (ptr) {
-		ptr = ptr -> next;
-		if (ptr->light == 0) {
-			count++;
-			countOp++;
-		}
-		if (ptr->light == 1) {
-			countOp++;
-			ptr->light = 0;
-			for (int i = count; i > 0; i--) {
-				ptr = ptr->prev;
-				countOp++;
-			}
-			if (ptr->light == 0) {
-				return count;
-			}
-			count = 1;
-		}
-	}
-	return -1;
+        Cage* ptr = first;
+        ptr->light = 1;
+        int count = 1;
+        while (ptr) {
+                ptr = ptr -> next;
+                if (ptr->light == 0) {
+                        count++;
+                        countOp++;
+                }
+                if (ptr->light == 1) {
+                        countOp++;
+                        ptr->light = 0;
+                        for (int i = count; i > 0; i--) {
+                                ptr = ptr->prev;
+                                countOp++;
+                        }
+                        if (ptr->light == 0) {
+                                return count;
+                        }
+                        count = 1;
+                }
+        }
+        return -1;
 }
